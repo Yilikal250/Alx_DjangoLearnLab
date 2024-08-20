6573c55452cb03d144ee84c3d65efb35a92393ad
Yilikal250 <teshomeyilikal250@gmail.com>
2024-08-20 22:04:34 +0200

    admin file added

--- 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/admin.py
+++ 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/admin.py
@@ -5,11 +5,17 @@ from .models import Book
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     # Display title, author, and publication year in the admin list view
-    list_display = ('title', 'author', 'published_date')
-    
+    list_display = ('title', 'author', 'publication_year')
+
     # Add filters for author and publication year
     list_filter = ('author', 'published_date')
-    
+
     # Enable search by title and author
     search_fields = ('title', 'author')
 
+    # Method to extract and display the publication year
+    def publication_year(self, obj):
+        return obj.published_date.year
+    
+    # Add short description for the custom method
+    publication_year.short_description = 'Publication Year'
