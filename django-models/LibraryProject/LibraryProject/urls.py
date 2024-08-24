01997f5a9d1abbeadbbf4c1b13158f893de09db6
Yilikal250 <teshomeyilikal250@gmail.com>
2024-08-24 20:46:03 +0200

    functions and clasess added

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import book_list, LibraryDetailView # type: ignore
+from .views import book_list, LibraryDetailView 
 urlpatterns = [
     path('admin/', admin.site.urls),
      # URL pattern for the function-based view that lists all books
