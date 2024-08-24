8a5abb2c92a52eded0997763b913e0c182d170ba
Yilikal250 <teshomeyilikal250@gmail.com>
2024-08-24 09:25:13 +0200

    model and relationship added

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,20 +1,28 @@
-from django.db import models
+from relationship_app.models import Author, Book, Library, Librarian
 
-author_name = "George Orwell"
-books_by_author = Book.objects.filter(author__name=author_name)
+# Query to get all books by a specific author
+def get_books_by_author(author_name):
+    try:
+        author = Author.objects.get(name=author_name)  # Fetch the author by name
+        books = Book.objects.filter(author=author)  # Get all books by the author
+        return books
+    except Author.DoesNotExist:
+        return None
 
+# Query to list all books in a library
+def list_books_in_library(library_name):
+    try:
+        library = Library.objects.get(name=library_name)  # Fetch the library by name
+        books = library.books.all()  # Get all books in the library
+        return books
+    except Library.DoesNotExist:
+        return None
 
-library_name = "Central Library"
-library = Library.objects.get(name=library_name)
-
-# List all books in the library
-books_in_library = library.books.all()
-
-# Again, assuming you know the library's name
-library_name = "Central Library"
-library = Library.objects.get(name=library_name)
-
-# Retrieve the librarian associated with the library
-librarian = library.librarian
-
-# This will give you the Librarian instance associated with the specified library
+# Query to retrieve the librarian for a library
+def get_librarian_for_library(library_name):
+    try:
+        library = Library.objects.get(name=library_name)  # Fetch the library by name
+        librarian = Librarian.objects.get(library=library)  # Get the librarian for the library
+        return librarian
+    except (Library.DoesNotExist, Librarian.DoesNotExist):
+        return None
