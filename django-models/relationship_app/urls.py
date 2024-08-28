f47c8a39ed93db5c5ac43c6895c7c263b0618014
Yilikal250 <teshomeyilikal250@gmail.com>
2024-08-29 00:51:50 +0200

    templates added

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import LibraryDetailView  # Import the class-based view
+from .views import list_books, BookDetailView, LibraryDetailView  # Import the views you need
 
 urlpatterns = [
-    # Add a URL pattern for the LibraryDetailView
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-# Map the URL to the BookDetailView  
+    path('books/', list_books, name='book_list'),  # Add the URL pattern for listing books
+    path('book/<int:pk>/', BookDetailView.as_view(), name='book_detail'),  # URL pattern for book details
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # URL pattern for library details
 ]
