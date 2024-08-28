537707980ae975ed4a493d07d2179de332971d07
Yilikal250 <teshomeyilikal250@gmail.com>
2024-08-28 23:31:21 +0200

    templates added

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,26 +1,24 @@
 from django.shortcuts import render
-from django.views.generic.detail import DetailView
-from .models import Library
-# Create your views here.from django.shortcuts import render
 from .models import Book
-
+from django.views.generic import DetailView
+from .models import Library
 def book_list(request):
-    # Retrieve all books from the database
-    books = Book.objects.all()
-
-    # Create a simple text list of book titles and authors
-    book_list = "\n".join([f"{book.title} by {book.author}" for book in books])
-
-    # Render the response with the list of books
-    return render(request, 'book_list.html', {'book_list': book_list})
+    """View to list all books with their titles and authors."""
+    books = Book.objects.all()  # Fetch all book instances from the database
+    context = {'books': books}  # Create a context dictionary to pass the books to the template
+    return render(request, 'relationship_app/list_books.html', context)
 class LibraryDetailView(DetailView):
-    model = Library
-    template_name = 'library_detail.html'
-    context_object_name = 'library'
+    model = Library  # The model that this view will display
+    template_name = 'relationship_app/library_detail.html'  # Specify the template name
 
     def get_context_data(self, **kwargs):
-        # Call the base implementation to get the context
+        # Get the default context data from the DetailView
         context = super().get_context_data(**kwargs)
-        # Add all books related to this library
-        context['books'] = self.object.books.all()
+        
+        # Get the specific library instance
+        library = self.get_object()
+        
+        # Add the list of books related to this library to the context
+        context['books'] = library.books.all()  # This uses the related_name 'books' to access related books
+        
         return context
